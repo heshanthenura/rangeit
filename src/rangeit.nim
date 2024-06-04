8fef99d9f13345b10a5968b5f4e2de28e10180f0
Heshan Thenura <heshanthenura@protonmail.com>
2024-06-04 15:06:53 +0530

    added usage

--- src/rangeit.nim
+++ src/rangeit.nim
@@ -1,5 +1,17 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import os
+
+proc printUsage() = 
+  echo"""
+  Usage:
+    rangeit network [range/cidr]      Calculate network address.
+    rangeit broadcast [range/cidr]    Determine broadcast address.
+    rangeit usable [range/cidr]       List all usable IP addresses.
+    rangeit details [range/cidr]      Get network, broadcast, and all usable addresses.
+  """
 
 when isMainModule:
-  echo("Hello, World!")
+  let args = commandLineParams()
+  
+  if args.len == 0:
+    printUsage()
+    quit(1)

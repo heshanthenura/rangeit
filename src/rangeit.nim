ae6d8356bab1375083bdd88e6491b411b73d7666
Heshan Thenura <heshanthenura@protonmail.com>
2024-06-05 22:49:08 +0530

    added getIndex method

--- src/rangeit.nim
+++ src/rangeit.nim
@@ -9,9 +9,17 @@ proc printUsage() =
     rangeit details [range/cidr]      Get network, broadcast, and all usable addresses.
   """
 
+proc getIndex(args: seq[string], target: string): int =
+  for i, arg in args:
+    if arg == target:
+      return i
+  return -1
+
 when isMainModule:
   let args = commandLineParams()
-  
+  echo "Number of arguments:",args.len
+  echo "Calc flag is in ", getIndex(args, "-calc")
+  echo args[getIndex(args, "-calc")+1]
   if args.len == 0:
     printUsage()
     quit(1)
